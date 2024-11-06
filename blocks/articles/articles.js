c17d74e4ca1254e5b7f120cb9db42f0ddccb6b63
Aadi Ahlawat <433586@conneqtcorp.com>
2024-11-06 18:12:04 +0530

    optimized the article component

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -1,97 +1,77 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-async function fetchArticles(block,path,articlesPerPage,currentPage) {
+
+const fetchArticles = async (path,articlesPerPage,currentPage) => {
     try {
     const offset=(currentPage-1)*articlesPerPage;
-    let articlesPath=path+"?offset="+offset+"&limit="+articlesPerPage;
+    let articlesPath=`${path}?offset=${offset}&limit=${articlesPerPage}`;
     const response = await fetch(articlesPath); // Replace with your JSON URL
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
-    renderData(block,data);
+    return await response.json();;
 } 
     catch (error) {
-        return [];
+        console.error('Error fetching articles:- ', error);
+        return { data: [], total: 0 };
     }
 }
+const  createArticleElement = ({Image, Title, Description}) =>
+{
+    const articleContent = document.createElement('article');
 
-function renderData(block,data)  
-{  
-    const articleWrapper = document.createElement('div');
-    articleWrapper.classList.add('articles-outer')
-    const articles=data.data;
-    const totalArticles=data.total;
-    articleWrapper.setAttribute('data-totalArticles',totalArticles);
+    const articleImageContainer=document.createElement('div');
+    articleImageContainer.classList.add('article-image-container');
+    const articleImage = document.createElement('img'); // Create an img element
+    articleImage.src = Image; // Set the image source 
+    articleImage.alt = Title;
+
+    articleImageContainer.appendChild(articleImage);
+    articleContent.appendChild(articleImageContainer);
     
-    articles.forEach(article => {
 
-        const articleContent = document.createElement('article');
+    const articleTitleContainer=document.createElement('div');
+    articleTitleContainer.classList.add('article-title-container');
+    const articleTitle = document.createElement('a'); // Create an a element
+    articleTitle.href = `magazine/${Title.toLowerCase().replace(/\s+/g, '-')}`; 
+    articleTitle.textContent = Title;
 
-        const articleImageContainer=document.createElement('div');
-        articleImageContainer.classList.add('article-image-container');
-        const articleImage = document.createElement('img'); // Create an img element
-        articleImage.src = article.Image; // Set the image source (replace with your image URL)
-        articleImage.alt = article.Title;
+    articleTitleContainer.appendChild(articleTitle);
+    articleContent.appendChild(articleTitleContainer);
 
-        articleImageContainer.appendChild(articleImage);
-        articleContent.appendChild(articleImageContainer);
-        
+    const articleDescriptionContainer=document.createElement('div');
+    articleDescriptionContainer.classList.add('article-description-container');
+    const articleDescription = document.createElement('p'); 
+    articleDescription.textContent = Description;
 
-        const articleTitleContainer=document.createElement('div');
-        articleTitleContainer.classList.add('article-title-container');
-        const articleTitle = document.createElement('a'); // Create an a element
-        articleTitle.href = "magazine/"+article.Title.toLowerCase().replace(/\s+/g, '-'); 
-        articleTitle.textContent = article.Title;
-
-        articleTitleContainer.appendChild(articleTitle);
-        articleContent.appendChild(articleTitleContainer);
-
-        const articleDescriptionContainer=document.createElement('div');
-        articleDescriptionContainer.classList.add('article-description-container');
-        const articleDescription = document.createElement('p'); // Create an a element
-        articleDescription.textContent = article.Description;
-
-        articleDescriptionContainer.appendChild(articleDescription);
-        articleContent.appendChild(articleDescriptionContainer);
+    articleDescriptionContainer.appendChild(articleDescription);
+    articleContent.appendChild(articleDescriptionContainer);
+    return articleContent;
+}
+const renderData = (block,data) =>
+{  
+    const articleWrapper = document.createElement('div',);
+    articleWrapper.classList.add('articles-outer');
+    articleWrapper.setAttribute('data-totalArticles',data.total);
+    
+    data.data.forEach(article => {
+        const articleContent=createArticleElement(article);
         articleWrapper.appendChild(articleContent);
-
     });
-    block.querySelector('div').style.display = 'none'; 
+
+    // optimize the images
     articleWrapper.querySelectorAll('img').forEach((img) => img.replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+    
+    block.querySelector('div').style.display = 'none'; 
 
     block.append(articleWrapper);
 }
-/*
-function renderPagination(block,path,articlesPerPage,totalArticles,currentPage)
-{
-    const totalPages=Math.ceil(totalArticles/articlesPerPage);
-    const pagination=document.createElement('ul');
-    pagination.classList.add('articles-pagination');
-    pagination.innerHTML='';
-    for(let i=1;i<=totalPages;i++)
-    {
-        const li=document.createElement('li');
-        li.textContent=i;
-        //li.classList.add(i===currentPage ? 'active' : '');
-        li.addEventListener('click',() =>{
-            currentPage=i;
-            paginate(block,path,articlesPerPage,totalArticles,currentPage);
-        });
-        pagination.appendChild(li);
-    }
-    block.append(pagination);
-} 
-async function paginate(block,path,articlesPerPage,totalArticles,currentPage)
-{
-    fetchArticles(block,path,articlesPerPage,currentPage);
-}*/
 export default async function decorate(block) {
-        let currentPage=1;
+
+        const currentPage=1;
         const articlesPerPage=8;
         const link = block.querySelector('a');
         const path = link ? link.getAttribute('href') : block.textContent.trim();
-        const articles=await fetchArticles(block,path,articlesPerPage,currentPage);
-        const totalArticles=block.querySelector('.articles-outer').getAttribute('data-totalArticles');
-       
-        //renderPagination(block,path,articlesPerPage,totalArticles,currentPage);
+        const articles=await fetchArticles(path,articlesPerPage,currentPage);
+        
+        renderData(block,articles);
 }
